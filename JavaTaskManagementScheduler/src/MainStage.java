51bed7d762d4332d049979465cc8f7565bed98ec
MarianoMirandaHerrera <marianomirandaherrera@gmail.com>
2024-08-24 12:15:18 -0700

    Project Initial Setup/Configuration
    Initialized Project and configured JavaFx library.

---
+++ JavaTaskManagementScheduler/src/MainStage.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainStage extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    
+}
